72a390742187f2ab7d04dca529ebc96be0f0bc60
Jaj <Jaj@WIN-11DNEK9BJP6.Home>
2017-01-30 17:33:25 +0000

    Working index page

--- judgecollege/src/main/java/com/fdmgroup/judgecollege/controller/HomeController.java
+++ judgecollege/src/main/java/com/fdmgroup/judgecollege/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.fdmgroup.judgecollege.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String index(){
-		return "Hello World";
+	public String index(Model model){
+		model.addAttribute("greeting", "Hello World");
+		model.addAttribute("tagline", "Welcome to Judge College");
+		return "index";
 	}
 
 }
