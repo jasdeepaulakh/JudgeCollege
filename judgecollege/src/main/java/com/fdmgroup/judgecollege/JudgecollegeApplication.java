80a8629dfef272a74e9013fd7cdcdcb6df304aad
Jaj <Jaj@WIN-11DNEK9BJP6.Home>
2017-01-30 14:04:37 +0000

    Initial Commit

---
+++ judgecollege/src/main/java/com/fdmgroup/judgecollege/JudgecollegeApplication.java
@@ -0,0 +1,12 @@
+package com.fdmgroup.judgecollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgecollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgecollegeApplication.class, args);
+	}
+}
---
+++ judgecollege/src/test/java/com/fdmgroup/judgecollege/JudgecollegeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fdmgroup.judgecollege;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JudgecollegeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
